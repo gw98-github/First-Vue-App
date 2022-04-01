4a248b8b1783c2a28fa4dde20e46d3b73889f599
gw98-github <grzegorz.wazny.pv@gmail.com>
2022-04-01 16:12:57 +0200

    prettier

--- src/components/component-list/ComponentList.vue
+++ src/components/component-list/ComponentList.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="info">Test</div>
-    
-    <div></div>
-    <div><router-link to="/starting-page"><button type="button" class="btn btn-primary">StartingPage</button></router-link></div>
+  <div class="info">Test</div>
+
+  <div></div>
+  <div>
+    <router-link to="/starting-page"
+      ><button type="button" class="btn btn-primary">
+        StartingPage
+      </button></router-link
+    >
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'ComponentList',
-
-}
+  name: "ComponentList",
+};
 </script>
 
 <style>
-.info{
-
+.info {
 }
 </style>
--- src/components/starting-page/StartingPage.vue
+++ src/components/starting-page/StartingPage.vue
@@ -1,21 +1,19 @@
 <template>
-    <div>  
-
-        <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+  <div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './HelloWorld.vue'
+import HelloWorld from "./HelloWorld.vue";
 
 export default {
-  name: 'StartingPage',
+  name: "StartingPage",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
-
 </style>
