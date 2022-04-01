04287b017660b91fea41bb0cb823bb0e7ceaf955
gw98-github <grzegorz.wazny.pv@gmail.com>
2022-04-01 14:40:51 +0200

    bootstrap

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <button type="button" class="btn btn-primary">Primary</button>
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
